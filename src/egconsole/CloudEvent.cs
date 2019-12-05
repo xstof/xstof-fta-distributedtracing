9a154c87e0fd5a93853e2749fb4fa56914097ec8
Ben Gimblett <begim@microsoft.com>
2019-12-05 15:44:48 +0000

    added console app to flow AI for EG AI test

--- src/egconsole/CloudEvent.cs
+++ src/egconsole/CloudEvent.cs
@@ -4,27 +4,8 @@ using System;
 
 namespace eg_webhook_api{
 
-    public class GridEvent<T> where T: class
-    {
-        [JsonPropertyName("id")]
-        public string Id { get; set;}
 
-        [JsonPropertyName("eventType")]
-        public string EventType { get; set;}
-
-        [JsonPropertyName("subject")]
-        public string Subject {get; set;}
-
-        [JsonPropertyName("eventTime")]
-        public DateTime EventTime { get; set; } 
-
-        [JsonPropertyName("data")]
-        public T Data { get; set; } 
-        [JsonPropertyName("topic")]
-        public string Topic { get; set; }
-    }
-
-    public class CloudEvent<T> where T : class
+  public class CloudEvent<T> where T : class
     {
 
         // Spec https://github.com/cloudevents/spec/blob/v1.0/spec.md
---
+++ src/egconsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using eg_webhook_api;
+using System.Text.Json;
+using Microsoft.ApplicationInsights.DependencyCollector;
+
+namespace egconsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = TelemetryConfiguration.CreateDefault();
+            var module = new DependencyTrackingTelemetryModule();
+            module.ExcludeComponentCorrelationHttpHeadersOnDomains.Add("core.windows.net");
+            module.Initialize(config);
+            config.InstrumentationKey = "2361c76b-4fe7-4349-bda1-c3fe73b3cefd";
+            config.TelemetryInitializers.Add(new HttpDependenciesParsingTelemetryInitializer());
+            var client = new TelemetryClient(config);
+
+            var submissionId = Guid.NewGuid().ToString();
+            var requestActivity = new Activity("Console App Start");
+            requestActivity.Start();
+            requestActivity.AddBaggage("MySubmissionId", submissionId);
+            var requestOperation = client.StartOperation<RequestTelemetry>(requestActivity);
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Add("ContentType","application/cloudevents+json");
+                httpClient.DefaultRequestHeaders.Add("aeg-sas-key","7dCVcy0te2hoXEb4lAc2UbUhEVL6RKgQPVqzEdDFqTA=");
+                var cloudEvent = new CloudEvent<dynamic>(){
+                    SpecVersion = "1.0",
+                    Type="com.example.someevent",
+                    Source="MyContent",
+                    Id ="A234-1234-1234",
+                    Time = DateTime.UtcNow.ToString(),
+                    DataContentType = "application/json",
+                    Data=null,
+                    TraceParent = requestActivity.Id,
+                    TraceState=$"MySubmissionId={submissionId}"
+                };
+                await httpClient.PostAsync("https://begim-egtopic-cs.westeurope-1.eventgrid.azure.net/api/events",new StringContent(JsonSerializer.Serialize(cloudEvent)));
+            }
+
+            client.TrackTrace($"Console App Closes");
+
+            client.StopOperation(requestOperation);
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
