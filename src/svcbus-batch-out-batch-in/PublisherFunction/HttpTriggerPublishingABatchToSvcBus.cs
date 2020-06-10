fbe3ab851afba8e1f869caf39e60ee80b5900e10
Christof <xstofgit@outlook.com>
2020-06-10 13:54:15 +0200

    use svc bus sdk directly to write out batch to svcbus

--- src/svcbus-batch-out-batch-in/PublisherFunction/HttpTriggerPublishingABatchToSvcBus.cs
+++ src/svcbus-batch-out-batch-in/PublisherFunction/HttpTriggerPublishingABatchToSvcBus.cs
@@ -10,11 +10,14 @@ using Newtonsoft.Json;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Azure.ServiceBus.Core;
+using System.Collections.Generic;
 
 namespace SvcbusBatchInBatchOut
 {
     public class HttpTriggerPublishingABatchToSvcBus
     {
+        private static MessageSender sendClient;
         private readonly TelemetryClient telemetryClient;
 
         public HttpTriggerPublishingABatchToSvcBus(TelemetryConfiguration telemetryConfiguration){
@@ -24,26 +27,29 @@ namespace SvcbusBatchInBatchOut
         [FunctionName("HttpTriggerPublishingABatchToSvcBus")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [ServiceBus("%ServiceBusQueueName%", Connection = "ServiceBusConnection")] IAsyncCollector<Message> messages,
+            // [ServiceBus("%ServiceBusQueueName%", Connection = "ServiceBusConnection")] IAsyncCollector<Message> messages,
             ILogger log)
         {
-            int batchSize = 100;  // publish a batch of 10 messages to service bus at a time
+            int batchSize = 100;  // publish a batch of 100 messages to service bus at a time
 
             log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // create the sendClient
+            var conn = Environment.GetEnvironmentVariable("ServiceBusConnection");
+            var queueName = Environment.GetEnvironmentVariable("ServiceBusQueueName");
+            if (sendClient == null || sendClient.IsClosedOrClosing)
+                sendClient = new MessageSender(conn, queueName);            
 
             var activityId = System.Diagnostics.Activity.Current.Id;
             System.Diagnostics.Activity.Current.AddTag("SampleName", "BatchOutBatchIn");
             System.Diagnostics.Activity.Current.AddBaggage("SampleName", "BatchOutBatchIn");
             System.Diagnostics.Activity.Current.AddTag("SampleActor", "BatchPublisher");
 
+            IList<Message> batchedMessages = new List<Message>();
             for(int i= 0; i<batchSize; i++){
                 var msgText = $"msg {i.ToString()}";
                 var msg = new Message(System.Text.UTF8Encoding.UTF8.GetBytes(msgText));
-
-                // Note: there is NO REAL BATCH SUPPORT
-                // see: https://github.com/Azure/azure-functions-servicebus-extension/blob/dev/src/Microsoft.Azure.WebJobs.Extensions.ServiceBus/Bindings/MessageSenderAsyncCollector.cs#L44
-                // this does not use: 
-                await messages.AddAsync(msg);
+                batchedMessages.Add(msg);
             }
 
             var metric = telemetryClient.GetMetric("NumberOfMessagesInBatchSubmitted");
@@ -51,33 +57,7 @@ namespace SvcbusBatchInBatchOut
 
             string responseMessage = $"This HTTP triggered function executed successfully and published a message batch onto Service Bus.  Activity.Current Id = {activityId}";
 
-            await messages.FlushAsync();
-            return new OkObjectResult(responseMessage);
-        }
-
-        [FunctionName("HttpTriggerPublishingSingleMsgToSvcBus")]
-        public IActionResult Run2(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [ServiceBus("%ServiceBusQueueName%", Connection = "ServiceBusConnection")] out Message message,
-            ILogger log)
-        {
-            int batchSize = 1;  // publish a batch of 10 messages to service bus at a time
-
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            var msgText = $"single msg";
-            message = new Message(System.Text.UTF8Encoding.UTF8.GetBytes(msgText));
-
-            var activityId = System.Diagnostics.Activity.Current.Id;
-            System.Diagnostics.Activity.Current.AddTag("SampleName", "BatchOutBatchIn");
-            System.Diagnostics.Activity.Current.AddBaggage("SampleName", "BatchOutBatchIn");
-            System.Diagnostics.Activity.Current.AddTag("SampleActor", "BatchPublisher");
-
-            var metric = telemetryClient.GetMetric("NumberOfMessagesInBatchSubmitted");
-            metric.TrackValue(batchSize);
-
-            string responseMessage = $"This HTTP triggered function executed successfully and published a message batch onto Service Bus.  Activity.Current Id = {activityId}";
-
+            await sendClient.SendAsync(batchedMessages);
             return new OkObjectResult(responseMessage);
         }
     }
